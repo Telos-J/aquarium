94eccbf8d2459335134e63e0ad909dbba8a34851
Dokyung Lee <dokyunglee@Dokyungs-MacBook-Air.local>
2020-11-21 17:49:26 +0900

    start alignment

--- formation.js
+++ formation.js
@@ -1,65 +1,69 @@
 const controlFish = new Fish(
-    Math.random() * canvas.width,
-    Math.random() * canvas.height
+  Math.random() * canvas.width,
+  Math.random() * canvas.height
 );
 controlFish.buildFish();
 controlFish.pieces.forEach((piece) => {
-    piece.color = 'yellow';
+  piece.color = "yellow";
 });
 
 const numFishes = 10;
-const fishes = [];
+const fishes = [controlFish];
 
 for (i = 0; i < numFishes; i++) {
-    const fish = new Fish(
-        Math.random() * canvas.width,
-        Math.random() * canvas.height
-    );
-    fish.buildFish();
-    fishes.push(fish);
+  const fish = new Fish(
+    Math.random() * canvas.width,
+    Math.random() * canvas.height
+  );
+  fish.buildFish();
+  fishes.push(fish);
 }
 
 const update = function () {
-    controlFish.move();
-    fishes.forEach((fish) => {
-        fish.move();
-    });
+  fishes.forEach((fish) => {
+    fish.move();
+  });
 
-    controlFish.avoidance.set(0, 0);
-    for (fish of fishes) {
-        if (controlFish.inNeighborhood(fish)) {
-            // avoidance, allignment, cohesion
-            controlFish.avoidance = controlFish.avoidance.add(
-                controlFish.position.sub(fish.position)
-            );
-        }
+  for (fish of fishes) {
+    fish.avoidance.set(0, 0);
+    for (otherFish of fishes) {
+      let fishAverage = otherFish.head / (numFishes + 1);
+
+      if (fish.inNeighborhood(otherFish)) {
+        // avoidance, allignment, cohesion
+        // fish.avoidance = fish.avoidance.add(
+        //   fish.position.sub(otherFish.position)
+        // );
+        fish.alignment = angleToVector(fishAverage);
+      }
     }
-    controlFish.avoidance = controlFish.avoidance.normalize(0.3);
+    // fish.avoidance = fish.avoidance.normalize(0.3);
+  }
 };
 
 const render = function () {
-    context.fillStyle = 'blue';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    drawNeighborhood(controlFish);
-    drawFish(controlFish);
-    fishes.forEach((fish) => {
-        drawFish(fish);
-    });
+  context.fillStyle = "blue";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawNeighborhood(controlFish);
+  drawFish(controlFish);
+  fishes.forEach((fish) => {
+    drawFish(fish);
+  });
 
-    for (fish of fishes) {
-        if (controlFish.inNeighborhood(fish)) {
-            context.strokeStyle = 'black';
-            context.moveTo(controlFish.position.x, controlFish.position.y);
-            context.lineTo(fish.position.x, fish.position.y);
-            context.stroke();
-        }
+  for (fish of fishes) {
+    if (controlFish.inNeighborhood(fish)) {
+      context.strokeStyle = "black";
+      context.moveTo(controlFish.position.x, controlFish.position.y);
+      context.lineTo(fish.position.x, fish.position.y);
+      context.stroke();
     }
+  }
 };
 
 const start = function () {
-    update();
-    render();
-    window.requestAnimationFrame(start);
+  update();
+  render();
+  window.requestAnimationFrame(start);
 };
 
 start();
